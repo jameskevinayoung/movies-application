87511a644ee3b11cae23988946685e6e6cdb9031
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-14 13:32:21 -0500

    movies app update

--- src/index.js
+++ src/index.js
@@ -37,6 +37,35 @@ function formatMovies(movie){//create a function that formats the movie title an
 
  */
 
+$('#submitButton').click(function(e){
+    e.preventDefault();
+
+
+    $.ajax('/api/movies', {
+        type: "POST",
+        dataType: "json",
+        data: {
+            title: $('#movieTitleInput').val(),
+            rating: $('input[name=rating]:checked').val()
+        }
+    });
+
+
+    $.ajax('/api/movies').done(function(data) {  //ajax request from api movies; when done run function
+        $('#info').html('');  //change html of #info
+        data.forEach(function(movie){ //loop through the data (an array with a forEach) getting each movie
+            $('#info').append(formatMovies(movie)); //append the #info with movie title
+        })
+    });
+    function formatMovies(movie){//create a function that formats the movie title and rating
+        let movieTitle = "<td class='movie'>"+ movie.title+"</td>";
+        let movieRating = "<td class='rating'>"+ movie.rating+"</td>";
+        let movieItem = "<tr class='rowcontent'>" + movieTitle+movieRating + "</tr>";
+        //variable that formats each line that will be added to the
+        return movieItem;
+    }
+});
+
 
 
 
